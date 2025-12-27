66b45bf3be5d7d0ff0935dfa5ea6e07ab1c399c2
Faisal Almasri <falmasri.ai@gmail.com>
2025-12-27 16:17:46 +0300

    fix tests and add python to workflow

--- c1/w3/tests/test_file.py
+++ c1/w3/tests/test_file.py
@@ -1,10 +1,10 @@
 import pytest
 def test_function():
-    assert 10 == 12
+    assert 10 == 10
 
 class TestClass:
     def test_method(self):
-        assert 10 + 5 == 1
+        assert 10 + 5 == 15
 
     @pytest.mark.parametrize("a, b",[(1,3),[5,2]])
     def test_sum(self,a,b):
